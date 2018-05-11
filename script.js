65ee79f16db5f1ad8d19ff08a98c7c8bb8fa33d2
Bilal Qazi <b_qazi@live.com>
2018-05-11 17:26:18 -0500

    added geocoding and fixed location error

--- script.js
+++ script.js
@@ -1,29 +1,60 @@
 const MEETUP_SEARCH_URL = 'https://api.meetup.com/find/upcoming_events';
 
-const MAPS_SEARCH_URL = 'dtjd';
+const MAPS_SEARCH_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 const MAPS_API_KEY = 'AIzaSyAPy0e7dVS6hPQaHYgSHd6n7cSPkkwxZt0';
 
 var map;
+
+var searchQuery;
+
+var searchLat;
+
+var searchLon;
 
 function setupQueryListener() {
 
 	$('#js-form').submit(function(event){
 		event.preventDefault();
 
-		const query = $('#js-topics').val();
+		searchQuery = $('#js-topics').val();
 		const location = $('#js-location').val();
-		console.log(query, location);
-		getDataFromApi(query, location);
+		console.log(searchQuery, location);
+		geocodeLocation(location);
 	});
 };
 
-function getDataFromApi(query, location) {
+function geocodeLocation(location) {
+	console.log(location);
+	const settings1 = {
+		url: MAPS_SEARCH_URL,
+		data: {
+			address: location,
+			key: MAPS_API_KEY
+		},
+		dataType: 'json',
+		type: 'GET',
+		success: getLatAndLong
+	};
+	$.ajax(settings1);
+};
+
+function getLatAndLong(data) {
+	console.log(data.results[0].geometry.location.lat, data.results[0].geometry.location.lng);
+	searchLat = data.results[0].geometry.location.lat;
+	searchLon = data.results[0].geometry.location.lng;
+	console.log(searchQuery, searchLat, searchLon);
+	getDataFromApi(searchQuery, searchLat, searchLon);
+};
+
+function getDataFromApi(searchQuery, lat, lon) {
+	
 	const settings = {
 		url: MEETUP_SEARCH_URL,
 		data: {
-			zip: location,
-			text: query,
+			lat: searchLat,
+			lon: searchLon,
+			text: searchQuery,
 			key: '501f552e7131947b686e2e3b1a149',
 			radius: 15
 		},
@@ -37,19 +68,20 @@ function getDataFromApi(query, location) {
 function initMap(){
 
 	map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 30.22, lng: -97.75},
+          center: {lat: searchLat, lng: searchLon},
           zoom: 12
     });
 };
 
 function displayAndRenderData(data) {
-	console.log(data.data.city.lat, data.data.city.lon);
+
 	initMap();
 	let eventsHtml = '';
 	for (let i=0; i < data.data.events.length; i++) {
 		let eachEvent = data.data.events[i];
 		let eachEventHtml = renderEvent(eachEvent);
 		eventsHtml += eachEventHtml;
+
 
 
 	}
@@ -74,3 +106,4 @@ function renderEvent(eachEvent){
 }
 
 $(setupQueryListener);
+
